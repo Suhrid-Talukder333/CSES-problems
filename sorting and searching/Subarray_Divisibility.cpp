02c0ed5dfc58e3d5aabc102f5140c3e7636d578d
Suhrid-Talukder333 <suhridtalukder333@gmail.com>
2021-05-01 23:59:25 +0600

    new

---
+++ sorting and searching/Subarray_Divisibility.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+ll pre[1000005], a[1000005];
+int main()
+{
+    ll n;
+    cin >> n;
+    memset(pre, 0, sizeof(pre));
+    pre[0] = 1;
+    //read the input
+    ll sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        sum += a[i];
+        sum %= n;
+        sum = (sum + n) % n;
+        pre[sum]++;
+    }
+    ll ans = 0;
+    for (int i = 0; i < n; i++)
+    {
+        ll m = pre[i];
+        ans += (m * (m - 1)) / 2;
+    }
+    cout << ans << endl;
+}
